77bf81ea40a5376f7212eb4244a644d972c440e2
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2020-11-19 10:27:35 +0700

    feat: :sparkles: follow substraction rule at level 1
    substraction cannot be less than 0

--- src/MathGame/Game/Pengurangan.java
+++ src/MathGame/Game/Pengurangan.java
@@ -17,4 +17,15 @@ public class Pengurangan extends Game {
     protected boolean assertAnswerIsCorrect(int answer, int numb1, int numb2) {
         return answer == numb1 - numb2;
     }
+
+    @Override
+    protected int[] generateQuizNumb() {
+        final int[] generated = super.generateQuizNumb();
+
+        if (this.level < 2)
+            if (generated[0] - generated[1] < 0)
+                return this.generateQuizNumb();
+
+        return generated;
+    }
 }
