2100698cc7a63c62994e764332914c776b7c03a6
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2020-12-07 00:05:42 +0700

    refactor: :art: split menu to Command Object

--- src/MahasiswaDB/MahasiswaDB.java
+++ src/MahasiswaDB/MahasiswaDB.java
@@ -8,17 +8,17 @@ import java.util.function.BiConsumer;
  * MahasiswaDB
  */
 public class MahasiswaDB {
-    final static Collection<Mahasiswa> mahasiswaCollection = new Collection<Mahasiswa>();
-    final static List<BiConsumer<Runnable, Runnable>> menus = Arrays.asList(
-        Command::exit,
-        Command::createMahasiswa,
-        Command::deleteMahasiswa,
-        Command::findByGender,
-        Command::findByNim,
-        Command::showData
-    );
+    final static Collection<Mahasiswa> MAHASISWA_COLLECTION = new Collection<Mahasiswa>();
+    final static List<Command> MENU = Arrays.asList(
+            new Command("Exit", MahasiswaDB::exit), 
+            new Command("Tambah Mahasiswa", MahasiswaDB::createMahasiswa),
+            new Command("Hapus Mahasiswa", MahasiswaDB::deleteMahasiswa), 
+            new Command("Cari Mahasiswa (by Gender)", MahasiswaDB::findByGender), 
+            new Command("Cari Mahasiswa (by NIM)", MahasiswaDB::findByNim), 
+            new Command("Tampil Mahasiswa", MahasiswaDB::showData)
+        );
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         MahasiswaDB.toMainMenu();
     }
 
@@ -28,9 +28,7 @@ public class MahasiswaDB {
         try {
             final int selection = Utils.inputInteger("Pilih menu: ");
 
-            MahasiswaDB.menus
-                .get(selection)
-                .accept(MahasiswaDB::toMainMenu, MahasiswaDB::exitProgram);
+            MahasiswaDB.MENU.get(selection).action.accept(MahasiswaDB::toMainMenu, MahasiswaDB::exitProgram);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Invalid Command! Please input available menu!");
             MahasiswaDB.toMainMenu();
@@ -43,41 +41,46 @@ public class MahasiswaDB {
 
     public static void showMenu() {
         System.out.println("Silahkan pilih menu berikut: (Mohon masukkan angka saja)");
-        System.out.println("1. Tambahkan Mahasiswa");
-        System.out.println("2. Hapus Mahasiswa");
-        System.out.println("3. Cari Mahasiswa");
-        System.out.println("4. Tampilkan Semua Data Mahasiswa");
-        System.out.println("0. Exit");
+        for (int i = 0; i < MENU.size(); i++)
+            System.out.println(i + ". " + MENU.get(i).title);
     }
 
-    public static class Command {
-        public static void createMahasiswa(Runnable next, Runnable exit) {
-            final MahasiswaCLI mahasiswaCLI = MahasiswaCLI.createMahasiswa();
+    public static void createMahasiswa(Runnable next, Runnable exit) {
+        final Mahasiswa newMahasiswa = MahasiswaCLI.createMahasiswa().mahasiswaInstance;
 
-            mahasiswaCollection.add(mahasiswaCLI.mahasiswaInstance);
-            System.out.println("Berhasil menambahkan!");
+        MAHASISWA_COLLECTION.add(newMahasiswa);
+        System.out.println("Berhasil ditambahkan!");
+        newMahasiswa.print();
 
-            next.run();
-        }
+        next.run();
+    }
 
-        public static void deleteMahasiswa(Runnable next, Runnable exit) {
-            next.run();
-        }
+    public static void deleteMahasiswa(Runnable next, Runnable exit) {
+        next.run();
+    }
 
-        public static void findByGender(Runnable next, Runnable exit) {
-            next.run();
-        }
+    public static void findByGender(Runnable next, Runnable exit) {
+        next.run();
+    }
 
-        public static void findByNim(Runnable next, Runnable exit) {
-            next.run();
-        }
+    public static void findByNim(Runnable next, Runnable exit) {
+        next.run();
+    }
 
         public static void showData(Runnable next, Runnable exit) {
             next.run();
         }
 
-        public static void exit(Runnable next, Runnable exit) {
-            exit.run();
+    public static void exit(Runnable next, Runnable exit) {
+        exit.run();
+    }
+    
+    static public class Command {
+        final String title;
+        final BiConsumer<Runnable, Runnable> action;
+        public Command(String title, BiConsumer<Runnable, Runnable> action) {
+            this.title = title;
+            this.action = action;
         }
     }
 }
