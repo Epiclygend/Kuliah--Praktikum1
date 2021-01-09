d2eae0afe5cee5a6a54dad17a526196adca6883b
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2021-01-09 20:58:21 +0700

    feat: :lipstick: improve karyawan.print() interface

--- src/KaryawanDB/Karyawan.java
+++ src/KaryawanDB/Karyawan.java
@@ -13,13 +13,14 @@ public class Karyawan implements HasId {
     }
 
     public String kode = UUID.randomUUID().toString();
-    public String nama = "";
-    public String alamat = "";
+    public String nama = "-";
+    public String alamat = "-";
     private LocalDate tanggalLahir = LocalDate.MIN;
     public Golongan golongan = Golongan.A;
     public boolean statusMenikah = false;
     public byte jumlahAnak = 0;
 
+    public GajiKaryawanCalculator gajiCalculator = new GajiKaryawanCalculator(this);
     private static DateTimeFormatter TANGGAL_LAHIR_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     @Override
@@ -45,12 +46,24 @@ public class Karyawan implements HasId {
 
     public void print() {
         TextFormatter.drawSeparator();
-        System.out.println("Nama\t\t= " + nama);
+        System.out.println("DATA KARYAWAN");
+        TextFormatter.drawSeparator();
+        System.out.println("Kode Karyawan\t= " + kode);
+        System.out.println("Nama Karyawan\t= " + nama);
         System.out.println("Alamat\t\t= " + alamat);
         System.out.println("Tanggal Lahir\t= " + getTanggalLahir());
         System.out.println("Golongan Darah\t= " + golongan);
         System.out.println("Status Menikah\t= " + getStatusMenikah());
         System.out.println("Jumlah Anak\t= " + jumlahAnak);
         TextFormatter.drawSeparator();
+        System.out.println("Gaji Pokok\\tt= Rp " + gajiCalculator.getGajiPokok());
+        System.out.println("Tunjangan Istri/Suami\t= Rp " + gajiCalculator.getTunjanganSuamiIstri());
+        System.out.println("Tunjangan Pegawai\t= Rp " + gajiCalculator.getTunjanganPegawai());
+        System.out.println("Tunjangan Anak\t\t= Rp " + gajiCalculator.getTunjanganAnak());
+        TextFormatter.drawSeparator();
+        System.out.println("Gaji Kotor\t= Rp " + gajiCalculator.getGajiKotor());
+        System.out.println("Potongan\t= Rp " + gajiCalculator.getPotongan());
+        TextFormatter.drawSeparator();
+        System.out.println("Gaji Bersih\t= Rp " + gajiCalculator.getGajiBersih());
     }
 }
