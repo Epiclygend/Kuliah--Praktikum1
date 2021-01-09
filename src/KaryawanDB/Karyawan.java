e4638b437e12b356eeebdb3da178007a20313741
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2021-01-09 14:35:48 +0700

    feat: :sparkles: add Karyawan tglLahir setter

--- src/KaryawanDB/Karyawan.java
+++ src/KaryawanDB/Karyawan.java
@@ -1,17 +1,23 @@
 package KaryawanDB;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
 public class Karyawan implements HasId {
     public enum GolonganDarah {
         A, B, AB, O;
     }
 
-    public String kode;
-    public String nama;
-    public String alamat;
-    public String tanggalLahir;
-    public GolonganDarah golonganDarah;
-    public boolean statusMenikah;
-    public byte jumlahAnak;
+    public String kode = "";
+    public String nama = "";
+    public String alamat = "";
+    private LocalDate tanggalLahir = LocalDate.MIN;
+    public GolonganDarah golonganDarah = GolonganDarah.A;
+    public boolean statusMenikah = false;
+    public byte jumlahAnak = 0;
+
+    private static DateTimeFormatter TANGGAL_LAHIR_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     @Override
     public String getId() {
@@ -22,10 +28,22 @@ public class Karyawan implements HasId {
         return statusMenikah ? "Sudah Menikah" : "Belum Menikah";
     }
 
+    public void setTanggalLahir(int YYYY, int MM, int DD) {
+        tanggalLahir = LocalDate.of(YYYY, MM, DD);
+    }
+
+    public String getTanggalLahir() {
+        return tanggalLahir.format(TANGGAL_LAHIR_FORMATTER);
+    }
+
+    public int getAge() {
+        return Period.between(tanggalLahir, LocalDate.now()).getYears();
+    }
+
     public void print() {
-        System.out.println("Nama\t= " + nama);
-        System.out.println("alamat\t= " + alamat);
-        System.out.println("Tanggal Lahir\t= " + tanggalLahir);
+        System.out.println("Nama\t\t= " + nama);
+        System.out.println("Alamat\t\t= " + alamat);
+        System.out.println("Tanggal Lahir\t= " + getTanggalLahir());
         System.out.println("Golongan Darah\t= " + golonganDarah);
         System.out.println("Status Menikah\t= " + getStatusMenikah());
         System.out.println("Jumlah Anaka\t= " + jumlahAnak);
