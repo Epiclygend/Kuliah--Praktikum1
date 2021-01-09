65eda2e6217033ff51f8a31ffd281b8b9a721c98
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2021-01-09 22:17:48 +0700

    feat: :lipstick: redesign printAll Karyawan

--- src/KaryawanDB/KaryawanCollection.java
+++ src/KaryawanDB/KaryawanCollection.java
@@ -1,7 +1,21 @@
 package KaryawanDB;
 
+import Utils.TextFormatter;
+
 public class KaryawanCollection extends Collection<Karyawan> {
     public void printAll() {
-        getCollection().forEach(karyawan -> karyawan.print());
+        TextFormatter.drawSeparator();
+        System.out.println("KODE\t\t" + "NAMA\t\t" + "GOL\t\t" + "USIA\t\t" + "STATUS NIKAH\t" + "JML. ANAK");
+        TextFormatter.drawSeparator();
+        getCollection().forEach(karyawan -> {
+            System.out.println(
+                karyawan.kode + "\t" +
+                karyawan.nama + "\t" +
+                karyawan.golongan + "\t" +
+                karyawan.getAge() + "\t" +
+                karyawan.getStatusMenikah() + "\t" +
+                karyawan.jumlahAnak + "\t"
+            );
+        });
     }
 }
