bd6da6a1434387a381e0f6992d8b1a2299640749
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2021-01-09 22:34:18 +0700

    fix: :ambulance: fix PERCENTAGE vars

--- src/KaryawanDB/GajiKaryawanCalculator.java
+++ src/KaryawanDB/GajiKaryawanCalculator.java
@@ -2,21 +2,23 @@ package KaryawanDB;
 
 import java.util.HashMap;
 import java.util.Map;
+// import java.util.function.Function;
+
 import KaryawanDB.Karyawan.Golongan;
 
 public class GajiKaryawanCalculator {
-    private static HashMap<Golongan, Integer> GAJI_POKOK_RULE = new HashMap<>(Map.of(
+    final static HashMap<Golongan, Integer> GAJI_POKOK_RULE = new HashMap<>(Map.of(
         Golongan.A, 5000000,
         Golongan.B, 6000000,
         Golongan.C, 7000000,
         Golongan.D, 8000000
     ));
-    private static float TUNJANGAN_SUAMI_ISTRI_PERCENTAGE = 10/100;
-    private static float TUNJANGAN_PEGAWAI_PERCENTAGE = 15/100;
-    private static float TUNJANGAN_ANAK_PERCENTAGE = 5/100;
-    private static float POTONGAN_GAJI_PERCENTAGE = 25/1000;
+    final static float TUNJANGAN_SUAMI_ISTRI_PERCENTAGE = 10/100f;
+    final static float TUNJANGAN_PEGAWAI_PERCENTAGE = 15/100f;
+    final static float TUNJANGAN_ANAK_PERCENTAGE = 5/100f;
+    final static float POTONGAN_GAJI_PERCENTAGE = 25/1000f;
 
-    private Karyawan karyawan;
+    final Karyawan karyawan;
 
     public GajiKaryawanCalculator(Karyawan karyawan) {
         this.karyawan = karyawan;
